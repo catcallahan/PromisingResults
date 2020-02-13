94800a0713eed55b03310256ecb1618f94d1658e
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-13 09:09:32 -0600

    complete promise chain

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+//---- Promise Chain ---//
 slowMath.add(6,2)
 .then ((a) => {
     console.log(a)
@@ -13,7 +15,25 @@ slowMath.add(6,2)
     return slowMath.add(d,98)
 }).then ((e) => {
     console.log(e);
-    return slowMath.divide(e,2)
+    return slowMath.remainder(e,2)
 }).then ((f) => {
-    console.log(f)
+    console.log(f);
+    return slowMath.multiply(f,50)
+}).then ((g)=> {
+    console.log(g);
+    return slowMath.remainder(g, 40);
+} ).then((h) => {
+    console.log(h);
+    return slowMath.add(h, 32);
+}).then((i) => {
+    console.log(`The final result is ${i}`)
+}).catch ((err)=> {
+    err = new Error ("The math doesn't pan out");
+    console.log(err);
 });
+
+//----Async and Await ---//
+
+
+
+
